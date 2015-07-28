72112a9c654f613473f004ac8b9a51fee5061d0f
smartdoc_jen <smartdoc_jen@bitbucket.org>
2015-07-28 10:00:50 +0800

    Initial Commit

---
+++ SocketTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocketTest
+//
+//  Created by xulu on 15/7/28.
+//  Copyright (c) 2015年 _MC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SocketTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocketTest
+//
+//  Created by xulu on 15/7/28.
+//  Copyright (c) 2015年 _MC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
